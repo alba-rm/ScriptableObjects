79dd715419316ad7b213ba5a096f84a7abdb888f
alba-rm <alba.r.m.722@gmail.com>
2024-03-15 12:31:41 +0100

    buttons

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -13,16 +13,39 @@ public class InventorySlot : MonoBehaviour
     public Text inspectionPrice;
     public Text inspectionDescription;
     public Button deleteButton;
+    public Button closeButton;
 
     public void InspectItem()
     {
         if(slotItem != null)
         {
+            deleteButton.onClick.AddListener(RemoveItem);
+            closeButton.onClick.AddListener(CloseInspectionWindow);
             inspectionImage.sprite = slotItem.itemSprite;
             inspectionName.text = slotItem.itemName;
             inspectionPrice.text = slotItem.itemPrice.ToString();
             inspectionDescription.text = slotItem.itemDescription;
+            
             inspectionWindow.SetActive(true);
         }
+        
+    }
+    public void RemoveItem()
+    {
+        if(InventoryManager.instance.weapons[slotNumber] != null)
+        {
+            InventoryManager.instance.weapons[slotNumber] = null;
+            InventoryManager.instance.weaponsNames[slotNumber].text = "Empty";
+            InventoryManager.instance.weaponsSprites[slotNumber].sprite = null;
+        }
+        slotItem = null;
+        deleteButton.onClick.RemoveListener(RemoveItem);
+        inspectionWindow.SetActive(false);
+    }
+
+    public void CloseInspectionWindow()
+    {
+        deleteButton.onClick.RemoveListener(RemoveItem);
+        inspectionWindow.SetActive(false);
     }
 }
